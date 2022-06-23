51e9b16f4b63108bb83097c3b10d6fb2d557f551
Kiran C.R <kirancr2000@gmail.com>
2022-06-24 01:25:33 +0530

    Add files via upload
    I just add two R programming code files

---
+++ 1.variables.R
@@ -0,0 +1,38 @@
+# Declaring variables and Assign values to them
+
+
+# use "<-" symbol to assign the values to the variables
+name <- "kiran C R"
+age<- 21
+marks <- 98.34
+
+#  assign same value to multiple variables
+text1<-text2<-text3<-"Engineering"
+
+# Using print() statement to print the variables values
+print(grade)
+print(age)
+
+
+#Creating array using range
+y <- 1:10
+print(y)   
+# 1 2 3 4 5 6 7 8 9 10    the values are started from 1 not 0
+
+# assign values simultaneously
+A <- B <- 1:7
+print(A)
+print(B)
+
+# c is concatenate the values
+z <- c(1,2,3,4,5,6)
+print(z)
+
+
+#it support the different data types
+d <- c("kiran", 21,98.3, F)
+print(d)
+
+#"kiran" "21"    "98.3"  "FALSE"
+
+
---
+++ 2.Data_Types.R
@@ -0,0 +1,69 @@
+# Data types in R-Programming
+
+# 1. Numeric 
+numeric_num <- 23
+numeric_num2  <- 28.9
+class(numeric_num)
+class(numeric_num2)
+
+#it prints  "numeric"
+# "class()" is used to print datatype of variables
+
+
+
+
+
+
+# 2. Integer 
+int_num <- 89L
+class(int_num)
+
+# it prints "integer"
+
+
+
+
+
+# 3. complex
+com_num <- 2+7i
+class(com_num)
+
+# it prints "complex"
+
+
+
+
+
+
+# 4. character
+char_data <- "kiran c r"
+char_data2 <- 'k'
+class(char_data)
+class(char_data2)
+
+# both are prints "character"
+
+
+
+# 5.logical
+log_data <- TRUE
+log_data1 <- FALSE
+log_data2 <- T
+log_data3 <- F
+
+class(log_data)
+class(log_data1)
+class(log_data2)
+class(log_data3)
+
+#all are printed "logical"
+
+
+
+
+# another method to check the data type of an variable
+typeof(log_data)
+
+
+
+
